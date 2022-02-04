1b7ce50676eeb2f64ec4ecb44de1e34c24807f80
Jose Miguel Garcia Quintero <josegarcia.developer@gmail.com>
2022-02-04 14:52:10 -0400

    adding tests

--- ui/src/components/form/__tests__/index.spec.js
+++ ui/src/components/form/__tests__/index.spec.js
@@ -1,10 +1,67 @@
-import { shallow } from 'enzyme';
+import { shallow } from "enzyme";
 
-import { BookmarkForm } from '../index';
+import { BookmarkForm } from "../index";
 
-describe('BookmarkForm', () => {
-    it('should match snapshot', () => {
-        const rendered = shallow(<BookmarkForm />);
-        expect(rendered).toMatchSnapshot();
-    });
+describe("BookmarkForm", () => {
+  let onHandleChangeMock, onHandleSaveMock;
+
+  beforeEach(() => {
+    onHandleChangeMock = jest.fn();
+    onHandleSaveMock = jest.fn();
+  });
+  let props = {
+    onHandleChange: onHandleChangeMock,
+    onHandleSave: onHandleSaveMock,
+    form: {
+      title: "",
+      link: "",
+    },
+  };
+
+  it("should match snapshot", () => {
+    const rendered = shallow(<BookmarkForm {...props} />);
+    expect(rendered).toMatchSnapshot();
+  });
+
+  it("should match Onchange Event", () => {
+    const rendered = shallow(
+      <BookmarkForm
+        {...props}
+        onHandleSave={onHandleSaveMock}
+        onHandleChange={onHandleChangeMock}
+      />
+    );
+
+    rendered
+      .find("input")
+      .at(0)
+      .simulate("change", {
+        target: { name: "title", value: "Test react app" },
+      });
+    rendered
+      .find("input")
+      .at(1)
+      .simulate("change", {
+        target: { name: "link", value: "http://link.com" },
+      });
+
+    rendered.find("form").simulate("submit");
+    expect(onHandleChangeMock).toHaveBeenCalled();
+    expect(onHandleSaveMock).toHaveBeenCalled();
+    expect(rendered).toMatchSnapshot();
+  });
+
+  it("should match Onchange empty", () => {
+    props.form.title = "test";
+    const rendered = shallow(
+      <BookmarkForm
+        {...props}
+        onHandleSave={onHandleSaveMock}
+        onHandleChange={onHandleChangeMock}
+      />
+    );
+
+    expect(onHandleSaveMock).toHaveBeenCalled();
+    expect(rendered).toMatchSnapshot();
+  });
 });
--- ui/src/components/list/__tests__/index.spec.js
+++ ui/src/components/list/__tests__/index.spec.js
@@ -3,8 +3,16 @@ import { shallow } from 'enzyme';
 import { BookmarkList } from '../index';
 
 describe('BookmarkList', () => {
+    let props={
+       data:[
+        {id:'48e6c5e7-e0c6-4614-8ac1-8193920de8eb',title:'google',link:'http://google.com'},
+        {id:'92ac82a5-74ba-4a99-9ede-d9d2a434568f',title:'Bookmarks',link:'http://Bookmarks.com'},
+        {id:'f1f20ffd-edf8-4f11-99ab-359158723eb1',title:'curso react native',link:'http://reactNative.com'},
+        {id:'bbda2eb6-6917-47f0-a512-5903cc568c44',title:'curso flutter',link:'http://flutterCurso.com'},
+       ]
+    }
     it('should match snapshot', () => {
-        const rendered = shallow(<BookmarkList />);
+        const rendered = shallow(<BookmarkList {...props}/>);
         expect(rendered).toMatchSnapshot();
     });
 });
